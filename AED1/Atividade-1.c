7bfb190b9154c7a4586c7fcf9e62c24c69222b58
Rafael Braga Ennes <111386423+rafaelbragae@users.noreply.github.com>
2022-09-19 11:55:07 -0300

    Add files via upload

--- AED1/Atividade-1.c
+++ AED1/Atividade-1.c
@@ -1,43 +1,189 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define MAX 100
 
-int main()
-{
-    int qtd, i;
-    int *N, *G;
-    int max=100;
-    int qtd_F[max],qtd_M[max];
-    int numUSA_M[max],numUSA_F[max];
+typedef struct {
+  int quantidade;
+  int qtdF[MAX], qtdM[MAX];
+  int Masculino;
+  int Feminino;
+  int Tamanho[MAX], Genero[MAX];
+} Sapatos;
 
-    scanf("%d", &qtd);
-    if(qtd>=100){
-        return 0;
+// Função para arredondamento do tamanho do sapato;
+float Arredondamento(float valor, int quantidade) {
+  int j;
+  for (j = 0; j < quantidade; j++) {
+    if ((valor - (int)valor) < 0.4) {
+      valor = (int)valor;
+    } else if ((valor - (int)valor) >= 0.4 && (valor - (int)valor) < 0.8) {
+      valor = (int)valor + 0.5;
+    } else if ((valor - (int)valor) >= 0.8) {
+      valor = (int)valor + 1;
     }
+  }
+  return valor;
+}
 
-    /*N = (int *)malloc(qtd * sizeof(int));
-    G = (int *)malloc(qtd * sizeof(int));*/
-    for (i = 0; i < qtd; ++i) {
-         scanf("%i %d", &N, &G);
-         N[i] = N;
-         G[i] = G;
+// Funções para converter o tamanho brasileiro para americano;
+float ConversaoMasculina(int valor, int quantidade) {
+  float conversao = 0, resultado;
+  conversao = ((((valor * 6.67) + 19) / 25.4) * 3) - 24;
+  resultado = Arredondamento(conversao, quantidade);
+  return resultado;
+}
 
+float ConversaoFeminina(int valor, int quantidade) {
+  float conversao = 0, resultado;
+  conversao = ((((valor * 6.67) + 19) / 25.4) * 3) - 23;
+  resultado = Arredondamento(conversao, quantidade);
+
+  return resultado;
+}
+
+// Eliminação dos números repetidos;
+float Eliminacao(int tam,float vetor[]){
+  int i, j, k;
+  for( i = 0; i < tam; i++ ){
+    for( j = i + 1; j < tam; ){
+      if( vetor[j] == vetor[i] ){
+        for( k = j; k < tam; k++ )
+          vetor[k] = vetor[k + 1];
+            tam--;
+          }
+      else{
+        j++;
+      }}}}
+
+// Código Principal;
+int main(){
+  float resultadoMasculino[MAX], resultadoFeminino[MAX];
+  Sapatos sapato;
+  int aux = 1;
+  sapato.Masculino = 0;
+  sapato.Feminino = 0;
+  scanf("%d", &sapato.quantidade);
+
+  int j = 0;
+  while (j != sapato.quantidade) {
+    scanf("%d", &sapato.Tamanho[j]);
+    scanf("%d", &sapato.Genero[j]);
+    j++;
+  }
+  
+// Conversão dos tamanhos dos sapatos femininos;
+  j = 0;
+  int i = 0;
+  while (j != sapato.quantidade) {
+    if (sapato.Genero[j] == 1) {
+      resultadoFeminino[i] = ConversaoFeminina(sapato.Tamanho[j], sapato.quantidade);
+      i++;
+      sapato.Feminino = sapato.Feminino+1;
     }
-    for (i = 0; i < qtd; ++i) {
-        printf("%i ", N[i]);
-        printf("%d ", G[i]);
+    j++;
+  }
+// Ordenação dos tamanhos dos sapatos femininos;
+  float F[MAX];
+  int maior, menor;
+  maior = 0;
+  for(i=0; i<sapato.Feminino; i++){
+    if (resultadoFeminino[i] > resultadoFeminino[maior]){
+      maior = i;
     }
-    printf("\n");
-    free(N);
-
-    /*if(G==0){
-        numUSA_M[max];
-    }else{
-        if(G==1)
-            numUSA_F[max];
-        else
-            return 0;}
-
-    printf("F %.1f-(%d) %.1f-(%d) %.1f-(%d) %.1f-(%d)\n", numUSA_F[0], qtd_F[0],numUSA_F[1], qtd_F[1], numUSA_F[2], qtd_F[2], numUSA_F[3], qtd_F[3]);
-    printf("M %.1f-(%d) %.1f-(%d) %.1f-(%d) %.1f-(%d)", numUSA_M[0], qtd_M[0], numUSA_M[1],qtd_M[1], numUSA_M[2], qtd_M[2], numUSA_M[3], qtd_M[3]);*/
-    return 0;
+  }
+  for(j=0; j<sapato.Feminino;j++){
+  menor = 0;
+    for(i=0; i<sapato.Feminino; i++){
+      if (resultadoFeminino[i] < resultadoFeminino[menor]){
+        menor = i;
+      }
+    F[j]= resultadoFeminino[menor];
+    }
+  resultadoFeminino[menor] = resultadoFeminino[maior];
+  }
+// Eliminação dos sapatos femininos iguais;
+  int k;
+  for( k = 0; k < sapato.Feminino; k++){
+      sapato.qtdF[k] = aux;
+  }
+  for( i = 0; i < sapato.Feminino ; i++ ){
+    for( j = i + 1; j < sapato.Feminino; ){
+      if( F[j] == F[i] ){
+        for( k = j; k < sapato.Feminino; k++ )
+          F[k] = F[k + 1];
+          sapato.qtdF[k-2] = aux+1;
+          sapato.Feminino--;
+          }
+      else{
+        j++;
+  }}}
+// Print dos sapatos femininos;
+  j = 0;
+  while (j != sapato.quantidade) {
+    if (sapato.Genero[j] == 1) {
+      printf("F ");
+      break;
+    }
+    j++;
+  }
+  for (k=0; k < sapato.Feminino; k++){
+      printf("%.1f-(%i) ", F[k], sapato.qtdF[k]);
+  }
+  printf("\n");
+// Conversão dos tamanhos dos sapatos masculinos;
+  j = 0;
+  i = 0;
+  while (j != sapato.quantidade) {
+    if (sapato.Genero[j] == 0) {
+      resultadoMasculino[i] = ConversaoMasculina(sapato.Tamanho[j], sapato.quantidade);
+      i++;
+      sapato.Masculino = sapato.Masculino+1;
+    }
+    j++;
+  }
+// Ordenação dos tamanhos dos sapatos masculinos;
+  float M[MAX];
+  maior = 0;
+    for(i=0; i<sapato.Masculino; i++){
+      if (resultadoMasculino[i] > resultadoMasculino[maior]){
+            maior = i;
+      }
+    }
+    for(j=0; j<sapato.Masculino;j++){
+    menor = 0;
+      for(i=0; i<sapato.Masculino; i++){
+        if (resultadoMasculino[i] < resultadoMasculino[menor]){
+            menor = i;
+        }
+    M[j]= resultadoMasculino[menor];
+      }
+    resultadoMasculino[menor] = resultadoMasculino[maior];
+    }
+// Eliminação dos sapatos masculinos iguais;
+  for( k = 0; k < sapato.Masculino; k++){
+      sapato.qtdM[k] = aux;
+  }
+  for( i = 0; i < sapato.Masculino ; i++ ){
+    for( j = i + 1; j < sapato.Masculino; ){
+      if( M[j] == M[i] ){
+        for( k = j; k < sapato.Masculino; k++ )
+          M[k] = M[k + 1];
+          sapato.qtdM[k-2] = aux+1;
+          sapato.Masculino--;
+        }
+      else{
+        j++;
+  }}}
+// Print dos sapatos masculinos;
+  j = 0;
+  while (j != sapato.quantidade) {
+    if (sapato.Genero[j] == 0) {
+      printf("M ");
+      break;
+    }
+    j++;
+  }
+  for (k=0; k < sapato.Masculino; k++){
+        printf("%.1f-(%i) ", M[k], sapato.qtdM[k]);
+  }
 }
